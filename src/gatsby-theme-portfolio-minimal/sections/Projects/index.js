4f83d3f0e748d568cef55af86693983cff442122
Mike Seay <mikejseay@gmail.com>
2022-07-16 11:40:16 -0700

    Trying disabling the animation for Projects

---
+++ src/gatsby-theme-portfolio-minimal/sections/Projects/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Animation } from '../../components/Animation';
+import { Section } from '../../components/Section';
+import { Slider } from '../../components/Slider';
+import { Button, ButtonType } from '../../components/Button';
+import { Project } from '../../components/Project';
+import { useLocalDataSource } from './data';
+import * as classes from './style.module.css';
+
+export function ProjectsSection(props) {
+    const response = useLocalDataSource();
+    const data = response.allProjectsJson.sections[0];
+
+    return (
+        <Section anchor={props.sectionId} heading={props.heading}>
+            <Slider additionalClasses={[classes.Projects]}>
+                {data.projects.map((project, key) => {
+                    return project.visible ? <Project key={key} index={key} data={project} /> : null;
+                })}
+            </Slider>
+            {data.button !== undefined && data.button.visible !== false && (
+                <Animation className={classes.MoreProjects} type="fadeIn">
+                    <Button
+                        type={ButtonType.LINK}
+                        externalLink={true}
+                        url={data.button.url}
+                        label={data.button.label}
+                    />
+                </Animation>
+            )}
+        </Section>
+    );
+}
